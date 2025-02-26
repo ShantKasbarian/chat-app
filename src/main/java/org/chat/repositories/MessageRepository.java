39a81c995ac3b258b18ce78e345229fe01c50a38
Shant Kasbarian <shantkasbarian06@gmail.com>
2025-02-26 12:07:10 +0400

    fixed bugs in message service where user was unable to send messages and see sent messages

--- src/main/java/org/chat/repositories/MessageRepository.java
+++ src/main/java/org/chat/repositories/MessageRepository.java
@@ -15,15 +15,23 @@ public class MessageRepository implements PanacheRepository<Message> {
         this.entityManager = entityManager;
     }
 
-    public List<Message> getMessages(int currentUserId, String receiverUsername) {
+    public List<Message> getMessages(int currentUserId, int receiverId) {
         return entityManager
                 .createQuery(
                         "from Message where recepient = :recepient and senderId = :sender",
                         Message.class
                 )
-                .setParameter("recepient", receiverUsername)
+                .setParameter("recepient", receiverId)
                 .setParameter("sender", currentUserId)
                 .getResultList();
+    }
+
+    public void save(Message message) {
+        entityManager.createQuery("insert into Message (message, senderId, recepient) values (:message, :sender, :recepient)")
+            .setParameter("message", message.getMessage())
+            .setParameter("sender", message.getSenderId())
+            .setParameter("recepient", message.getRecepient())
+            .executeUpdate();
     }
 
     public Message writeMessage(int currentUserId, Message message, int groupId) {
