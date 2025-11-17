02dedcf58247e70bec1f0d7f6e883395c22bee66
Shant Kasbarian <shantkasbarian06@gmail.com>
2025-11-17 18:19:04 +0400

    replaced raw error string messages with dtos

--- src/main/java/org/chat/exception/ExceptionHandler.java
+++ src/main/java/org/chat/exception/ExceptionHandler.java
@@ -1,57 +1,58 @@
-package org.chat.config;
+package org.chat.exception;
 
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.ExceptionMapper;
 import jakarta.ws.rs.ext.Provider;
-import org.chat.exception.*;
+import org.chat.model.ErrorMessageDto;
 
 @Provider
 public class ExceptionHandler implements ExceptionMapper<Throwable> {
+    private static final String INTERNAL_SERVER_ERROR_MESSAGE = "internal server error";
+
     @Override
     public Response toResponse(Throwable throwable) {
         switch (throwable) {
             case InvalidCredentialsException e -> {
                 return Response.status(Response.Status.UNAUTHORIZED)
-                        .entity(e.getMessage())
+                        .entity(new ErrorMessageDto(e.getMessage()))
                         .build();
             }
 
             case InvalidRoleException e -> {
                 return Response.status(Response.Status.FORBIDDEN)
-                        .entity(e.getMessage())
+                        .entity(new ErrorMessageDto(e.getMessage()))
                         .build();
             }
 
             case ResourceNotFoundException e -> {
                 return Response.status(Response.Status.NOT_FOUND)
-                        .entity(e.getMessage())
+                        .entity(new ErrorMessageDto(e.getMessage()))
                         .build();
             }
 
             case InvalidGroupException e -> {
                 return Response.status(Response.Status.BAD_REQUEST)
-                        .entity(e.getMessage())
+                        .entity(new ErrorMessageDto(e.getMessage()))
                         .build();
             }
 
             case InvalidInfoException e -> {
                 return Response.status(Response.Status.BAD_REQUEST)
-                        .entity(e.getMessage())
+                        .entity(new ErrorMessageDto(e.getMessage()))
                         .build();
             }
 
             case UnableToJoinGroupException e -> {
                 return Response.status(Response.Status.CONFLICT)
-                        .entity(e.getMessage())
+                        .entity(new ErrorMessageDto(e.getMessage()))
                         .build();
             }
 
             default -> {
                 return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
-                        .entity(throwable.getMessage())
+                        .entity(new ErrorMessageDto(INTERNAL_SERVER_ERROR_MESSAGE))
                         .build();
             }
-
         }
     }
 }
